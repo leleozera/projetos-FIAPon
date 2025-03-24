f3bcab382683d7885b26fd355469a729a3451d07
leleozera <leonunesurbano@gmail.com>
2025-03-24 19:57:57 -0300

    Add files via upload

---
+++ aplicaçãoemR.R
@@ -0,0 +1,16 @@
+# cálculo da média
+valores <- c("area_café", "insumo_cafe", "area_tomate","insumo_tomate")
+
+média <- mean(346.19, 500, 1000, 400)
+ 
+resultado_media <- mean(valores)
+
+print(resultado_media)
+
+# cálculo desvio padrão
+desvio_padrao <- sd(346.19, 500, 1000, 400)
+ 
+resultado_desvio_padrao <- sd(valores)
+
+print(resultado_desvio_padrao)
+
